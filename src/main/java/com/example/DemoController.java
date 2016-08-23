c306d00275cff34efc53ded607a936372d1019c8
lm <631663525@qq.com>
2016-08-23 18:26:26 +0800

    feat:添加第一个controller,编写第一个api

---
+++ src/main/java/com/example/DemoController.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Ming on 2016/8/23.
+ */
+@Controller
+@RequestMapping("/api")
+public class DemoController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String test(){
+        return "Hello world,明明";
+    }
+}
