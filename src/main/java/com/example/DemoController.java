02ae8d636838be1dd5ee88bb21fb9e30ab95bef4
lm <631663525@qq.com>
2016-08-25 10:51:45 +0800

    feat:spring boot 不可忽视之logger

--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,12 +11,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by Ming on 2016/8/23.
  */
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/")
 public class DemoController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/api/hello", method = RequestMethod.GET)
     @ResponseBody
     public String test(){
         return "Hello world,明名名米国";
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String log(){
+        logger.info("hello logger type={}","info");
+        logger.warn("hello logger type={}","warn");
+        logger.error("hello logger type={}","error");
+        logger.debug("hello logger type={}","debug");
+        return "Hello world";
+    }
+
+
 }
