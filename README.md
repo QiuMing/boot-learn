6b1e6030310395fddb00e87124e9e17ef3b796d0
lm <631663525@qq.com>
2016-08-25 21:45:05 +0800

    feat:为环境而生的profile,在maven properties 中见证profile的使用

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 ## [spring boot 之 properties](http://docs.spring.io/spring-boot/docs/current/reference/html/common-application-properties.html)
 
 ## [spring-boot banner](http://www.ekiras.com/2016/02/adding-custom-banner-in-springboot-application.html)
+
+## [spring boot 之 profile]()
+    
+    * mvn clean package -Dmaven.test.skip=true -P prod  
+
+    * mvn  spring-boot  run -P prod
+    
+    *  java -jar  ***.jar  --spring.profiles.active=dev
+    
+    * idea :    -Dspring.profiles.active=dev
+    
+* [Upgrading from Spring Boot 1.4.0 M1 更新日志](https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-1.4.0-M2-Release-Notes)
+
+
--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -2,28 +2,31 @@ package com.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Ming on 2016/8/23.
  */
-@Controller
+@RestController
 @RequestMapping("/")
 public class DemoController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Resource
+    private Environment env;
+
     @RequestMapping(value = "/api/hello", method = RequestMethod.GET)
-    @ResponseBody
     public String test(){
         return "Hello world,明名名米国";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
     public String log(){
         logger.info("hello logger type={}","info");
         logger.warn("hello logger type={}","warn");
@@ -32,5 +35,10 @@ public class DemoController {
         return "Hello world";
     }
 
+
+    @RequestMapping("/testProfile")
+    public String testProfile(){
+        return env.getProperty("profile");
+    }
 
 }
